f5b7bc9ef88e8f7277e1db2f3732bd0b955df4f2
McCrearyD <dyllanmccreary@gmail.com>
2018-11-12 18:59:05 -0800

    Added empty brain template & empty brain for stagnant agent

--- environment.py
+++ environment.py
@@ -2,6 +2,7 @@ import arcade
 import sys
 import math
 from actors import pawn, short_range_pawn, long_range_pawn
+from models import brain
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
@@ -11,6 +12,10 @@ class Environment(arcade.Window):
     def __init__(self, pawns):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT)
         arcade.set_background_color(arcade.color.BLACK)
+
+        for pawn in pawns:
+            pawn.set_env(self)
+
         self.pawns = pawns
         self.lasers = []
 
@@ -37,6 +42,20 @@ class Environment(arcade.Window):
                 for l in lpawn.get_lasers():
                     lasers.append(l)
         return lasers
+
+    def get_pawns(self, pawn):
+        """
+        Returns all opposing pawns.
+        @param pawn The calling pawn.
+        """
+
+        pawns = []
+
+        for p in self.pawns:
+            if p != pawn:
+                pawns.append(p)
+
+        return pawns
 
     def kill_pawn(self, pawn):
         """
@@ -83,7 +102,7 @@ def default_player_pawn():
     Generates a default player-controlled pawn with a default controller scheme.
     """
 
-    return pawn.Pawn(SCREEN_WIDTH * 0.2, SCREEN_HEIGHT / 2, (arcade.key.A, arcade.key.W, arcade.key.D, arcade.key.S), (arcade.key.LEFT, arcade.key.RIGHT), arcade.key.SPACE)
+    return pawn.Pawn(None, SCREEN_WIDTH * 0.2, SCREEN_HEIGHT / 2, (arcade.key.A, arcade.key.W, arcade.key.D, arcade.key.S), (arcade.key.LEFT, arcade.key.RIGHT), arcade.key.SPACE)
 
 
 def default_mindless_pawn():
@@ -91,7 +110,7 @@ def default_mindless_pawn():
     Generates a stagnant, brainless pawn.
     """
 
-    out = pawn.Pawn(SCREEN_WIDTH * 0.8, SCREEN_HEIGHT / 2)
+    out = pawn.Pawn(brain.Brain, SCREEN_WIDTH * 0.8, SCREEN_HEIGHT / 2)
     out.dir = math.pi
     return out
 
